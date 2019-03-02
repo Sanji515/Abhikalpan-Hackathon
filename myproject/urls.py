863b477e0229077b924f4af7f2754f5fca367e81
Sanjeev Singh <sanjeevsinghdevil97@gmail.com>
2019-03-02 17:06:44 +0530

    structured code

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', include('index.urls', namespace='index')),
+    path('', include('index.urls', namespace='index')),
 ]
