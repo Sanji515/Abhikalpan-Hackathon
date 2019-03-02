0ee4b2b18c50596611bad54ecbc1cb305b222e88
Anurag Bisht <bisht.anu05@gmail.com>
2019-03-02 17:06:38 +0400

    custom user registration model created

--- apps/index/views.py
+++ apps/index/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
-
+from accounts.forms import RegistrationForm
 # Create your views here.
 
 def index(request):
 
 	return render(request, 'index/index.html', {})
+
+def register(request):
+	if request.method == 'POST':
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/index');
+
+	else:
+		form = RegistrationForm()
+		args = {'form':form}
+		return render(request, 'index/index.html', args) ;
